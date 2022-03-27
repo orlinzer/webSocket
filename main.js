33aad7c1e48566445d6eb1f9d47c50586a9b2123
Or Linzer <orlinzer@gmail.com>
2022-03-27 16:48:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+console.log("Hello World!");
+
+let http = require('http');
+
+let server = http.createServer(function (request, response) {
+  response.writeHead(200);
+  response.write('Welcome');
+  response.end();
+});
+server.listen(80);
+
+let WebSocketServer = require('websocket').server;
+
+let wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  return true;
+}
+
+wsServer.on('request', function(request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    }
+    else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
+  });
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
+});
